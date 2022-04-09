9ebe1e4f66a647955e5ec95bf07e9747e563142f
nader <nadersalah20201111@gmail.com>
2022-04-10 00:55:29 +0200

    project initializing

---
+++ android/app/src/main/kotlin/com/nader/map_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nader.map_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Map Task',
+      // initialRoute: AppRoutes.mapScreen,
+      // getPages: AppRoutes.routes,
+    );
+  }
+}
